93c2cb98fbd42f8c8c65a489c403c44175e8c6e4
Sinan YILMAZ <snnylmz09@gmail.com>
2017-04-08 20:24:00 +0300

    Update MyFirebaseMessagingService.java

--- app/src/main/java/app/sinan/yilmaz/firebase/cloudchat/MyFirebaseMessagingService.java
+++ app/src/main/java/app/sinan/yilmaz/firebase/cloudchat/MyFirebaseMessagingService.java
@@ -19,20 +19,12 @@ import com.google.firebase.udacity.cloudchat.R;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-    private static final String TAG = "MyFirebaseMsgService";
+    private static final String TAG = "FirebaseMessagingServce";
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
         String notificationTitle = null, notificationBody = null;
-        String dataTitle = null, dataMessage = null;
-
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData().get("message"));
-            dataTitle = remoteMessage.getData().get("title");
-            dataMessage = remoteMessage.getData().get("message");
-        }
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
@@ -43,31 +35,28 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
-        sendNotification(notificationTitle, notificationBody, dataTitle, dataMessage);
+        sendNotification(notificationTitle, notificationBody);
     }
 
 
-       private void sendNotification(String notificationTitle, String notificationBody, String dataTitle, String dataMessage) {
+       private void sendNotification(String notificationTitle, String notificationBody) {
         Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("title", dataTitle);
-        intent.putExtra("message", dataMessage);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true)   //Automatically delete the notification
+                .setSmallIcon(R.mipmap.ic_launcher) //Notification icon
+                .setContentIntent(pendingIntent)
                 .setContentTitle(notificationTitle)
                 .setContentText(notificationBody)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
+                .setSound(defaultSoundUri);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
     }
 }
-
