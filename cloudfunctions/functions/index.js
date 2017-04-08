e61abeb2cba3530171e0cb7ac0377632eaa598aa
SinanYilmaz9 <snnylmz09@gmail.com>
2017-04-08 07:36:13 +0300

    cloudchat

---
+++ cloudfunctions/functions/index.js
@@ -0,0 +1,33 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+//
+admin.initializeApp(functions.config().firebase);
+
+// Listens for new messages added to messages/:pushId
+exports.pushNotification = functions.database.ref('/messages/{pushId}').onWrite( event => {
+
+    console.log('Push notification event triggered');
+
+    //  Grab the current value of what was written to the Realtime Database.
+    var valueObject = event.data.val();
+
+    if(valueObject.photoUrl != null) {
+      valueObject.photoUrl= "Sent you a photo!";
+    }
+    const payload = {
+        notification: {
+            title:valueObject.name,
+            body: valueObject.text || valueObject.photoUrl,
+            sound: "default"
+        },
+    };
+
+    const options = {
+        priority: "high",
+        timeToLive: 60 * 60 * 24
+    };
+
+
+    return admin.messaging().sendToTopic("pushNotifications", payload, options);
+});
